0e5ca200ad47902f2bd178c761bfd140448c987e
Breno Martins <breno.sique@hotmail.com>
2025-04-23 13:35:06 -0300

    Começo do Sistema ProjectCare Versão ALPHA

---
+++ ProjectCare/src/main/java/com/example/ProjectCare/ProjectCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCareApplication.class, args);
+	}
+
+}
---
+++ ProjectCare/src/test/java/com/example/ProjectCare/ProjectCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
