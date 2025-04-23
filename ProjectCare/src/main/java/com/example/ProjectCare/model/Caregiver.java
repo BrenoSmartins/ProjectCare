10334407a6eb6c05bf38e41e55e47e40798f4b26
Breno Martins <breno.sique@hotmail.com>
2025-04-23 15:55:38 -0300

    banco de dados v.01

---
+++ ProjectCare/src/main/java/com/example/ProjectCare/model/Caregiver.java
@@ -0,0 +1,4 @@
+package com.example.ProjectCare.model;
+
+public class caregiver {
+}
---
+++ ProjectCare/src/main/java/com/example/ProjectCare/model/Responsible.java
@@ -0,0 +1,14 @@
+package com.example.ProjectCare.model;
+// Classe do cliente que vai colocar os dados do idoso e procurar um cuidador compativel para sua necessidade.
+
+import jakarta.persistence.Entity;
+
+@Entity
+
+public class Client {
+    private long id;
+
+    private String name;
+
+
+}
